a9819ebbd4a630312e8b51e63d163cd8118c39fa
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-09-26 19:33:09 +0200

    initial version

---
+++ src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplication.java
@@ -0,0 +1,12 @@
+package at.hochbichler.microservices.udemycurrencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyCurrencyConversionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyCurrencyConversionServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package at.hochbichler.microservices.udemycurrencyconversionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyCurrencyConversionServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
