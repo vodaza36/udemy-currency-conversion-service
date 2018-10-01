d9eee91f17ae61916e274deefeb4225ae7172d71
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-01 18:21:41 +0200

    add sleuth

--- src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/CurrencyConversionController.java
+++ src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,7 @@
 package at.hochbichler.microservices.udemycurrencyconversionservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
 
@@ -41,6 +45,7 @@ public class CurrencyConversionController {
     public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
 
         CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+        logger.info("{}", response);
 
         return new CurrencyConversionBean(
                 response.getId(),
