33722db8b8dfe7f5cc910ad860d129a58bbfcebd
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-01 16:01:29 +0200

    add eureka

--- src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplication.java
+++ src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package at.hochbichler.microservices.udemycurrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("at.hochbichler.microservices.udemycurrencyconversionservice")
+@EnableDiscoveryClient
 public class UdemyCurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
