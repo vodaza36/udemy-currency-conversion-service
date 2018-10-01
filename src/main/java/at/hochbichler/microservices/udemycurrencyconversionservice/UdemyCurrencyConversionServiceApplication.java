8d177f50a93483b073a42fb978448f53c16c62c6
Thomas Hochbichler <thomas.hochbichler.bp@nttdata.com>
2018-10-01 14:22:04 +0200

    add open feign client

--- src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplication.java
+++ src/main/java/at/hochbichler/microservices/udemycurrencyconversionservice/UdemyCurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package at.hochbichler.microservices.udemycurrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("at.hochbichler.microservices.udemycurrencyconversionservice")
 public class UdemyCurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
